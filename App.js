f5b9383658003d99ab33c960a7651341159d1984
Gareth Preston <gareth@gpresto.f9.co.uk>
2021-10-26 20:45:47 +0100

    Creating the next deliveroo

--- App.js
+++ App.js
@@ -1,21 +1,123 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { View, Text, StyleSheet, Image } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NativeBaseProvider, Button, Input, Heading } from 'native-base'
+import pizzaImage from './assets/pizza.png'
 
-export default function App() {
+function HomeScreen({ route, navigation }) {
+  const [userEmail, setEmailAddress] = React.useState('');
+  const [storedEmail, setStoredEmail] = React.useState('');
+
+  let onPress = () => {
+    let params = { email: userEmail, exists: false }
+    if(userEmail == storedEmail) {
+      params.exists = true
+      navigation.navigate('Welcome', params)
+    }
+    else {
+      setStoredEmail(userEmail)
+      navigation.navigate('Welcome', params)
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ styles.container }>
+      <View style={ styles.greenRow }>
+        <Image source={ pizzaImage } style={ styles.image } />
+      </View>
+      <View style={ styles.whiteRow }>
+        <Heading size="xl">Mario's Pizza</Heading>
+        <Text size="lg">The best pizza in Belfast</Text>
+      </View>
+      <View style={ styles.redRow }>
+        <Input
+          placeholder='Email address'
+          value={ userEmail }
+          onChangeText={ email => setEmailAddress(email) }
+          style={ styles.textBox }
+        />
+        <Button 
+          style={ styles.button } 
+          bg="primary.400"
+          size="md" 
+          onPress={ onPress } block bordered primary>
+          Get updates from Mario's Pizza
+        </Button>
+      </View>
     </View>
+  );
+}
+
+function WelcomeScreen({ route, navigation }) {
+  const { email, exists } = route.params
+
+  return (
+    <View style={ styles.container }>
+      {!exists && <Heading size="xl">Welcome to Mario's</Heading>}
+      {!exists && <Text size="lg">We will send updates to {email.toLowerCase().trim()}</Text>}
+      {exists && <Text size="lg">Your {email.toLowerCase().trim()} is already in our mailing list</Text>}
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={ HomeScreen } />
+          <Stack.Screen name="Welcome" component={ WelcomeScreen } />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#F9423A',
+    padding: 0,
+  },
+  redRow: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F9423A',
+    width: '100%',
+  },
+  whiteRow: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    width: '100%',
+  },
+  greenRow: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#47894b',
+    width: '100%',
+  },
+  textBox: {
+    backgroundColor: 'white',
+    width: 250,
+    marginBottom: 5,
+  },
+  button: {
+    backgroundColor: '#D3D3D3',
+    width: 250,
+  },
+  image: {
+    width: 405, 
+    height: 259,
   },
 });
+
+export default App;
